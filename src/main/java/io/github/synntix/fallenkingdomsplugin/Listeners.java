c3acdbe2ba1837228a571535bf416c66a375ab87
Synntix <alexandre.legendre38@gmail.com>
2020-03-28 21:36:37 +0100

    Removed useless import

--- src/main/java/io/github/synntix/fallenkingdomsplugin/Listeners.java
+++ src/main/java/io/github/synntix/fallenkingdomsplugin/Listeners.java
@@ -12,8 +12,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.util.ArrayList;
-
 public class Listeners implements Listener {
 
     @EventHandler
